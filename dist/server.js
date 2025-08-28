b6ef096a23c8cbcef8304504df35741a45c49f73
Nurislom <anurislom425@gmail.com>
2025-08-28 18:58:44 +0500

    BRR: initial setup

---
+++ dist/server.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const moment_1 = __importDefault(require("moment"));
+console.log("EXECUTED!");
+const currentTime = (0, moment_1.default)().format("DD-MM-YYYY");
+console.log(currentTime);
---
+++ src/server.ts
@@ -0,0 +1,4 @@
+import moment from "moment";
+console.log("EXECUTED!");
+const currentTime = moment().format("DD-MM-YYYY");
+console.log(currentTime);
